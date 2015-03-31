2948323dae03f4ff28c427a504e9e1ac6df3b132
Romain SOMMERARD <rsommerard@gmail.com>
2015-03-31 18:01:37 +0200

    Update

--- rand/main.py
+++ rand/main.py
@@ -20,29 +20,23 @@ def bin_maker(a, b, c):
     p = p<<16 | c
     return p
 
-def is_good_seed(seed, key, iv):
-    srand(seed)
-    key0 = rand()
-    if(key0 == key[0]):
-        print("key0 == key[0]")
-        key1 = rand()
-        if(key1 == key[1]):
-            print("key1 == key[1]")
-            key2 = rand()
-            if(key2 == key[2]):
-                print("key2 == key[2]")
-                key3 = rand()
-                if(key3 == key[3]):
-                    print("key3 == key[3]")
-                    iv0 = rand()
-                    if(iv0 == iv[0]):
-                        print("iv0 == iv[0]")
-                        iv1 = rand()
-                        if(iv1 == iv[1]):
-                            print("iv1 == iv[1]")
-                            return True
+def XGCD(a, b):
+    u = (1, 0)
+    v = (0, 1)
 
-    return False
+    while(b != 0):
+        q, r = divmod(a, b)
+        a = b
+        b = r
+        tmp = (u[0] - q * v[0], u[1] - q * v[1])
+        u = v
+        v = tmp
+
+    return a, u[0], u[1]
+
+def modinv(a, b):
+    g, x, y = XGCD(a, b)
+    return x
 
 if __name__ == '__main__':
     rand = Rand()
@@ -71,58 +65,93 @@ if __name__ == '__main__':
 
     print("iv1: " + str(iv1))
     print('-' * 80)
-    ########################################
 
     for i in iv1:
         rand.srand(i)
         print(str((i // 2**16) % 2**15) + " | " + str(rand.rand()))
 
+    print('-' * 80)
+    ########################################
+
     iv0 = []
     for i in iv1:
-        iv0.append(divmod((i - 12345), 1103515245))
+        inva = modinv(1103515245, 2**32)
+        tmp = inva * (i - 12345) % 2**32
+        iv0.append(tmp)
 
-    print(iv0)
+    print("iv0: " + str(iv0))
+    print('-' * 80)
 
-    print(iv1[0])
-    t = iv1[0] * 1103515245 + 12345
-    print(t)
-    n = divmod((t - 12345), 1103515245)
-    print(n)
+    for i in iv0:
+        rand.srand(i)
+        print(str((i // 2**16) % 2**15) + " | " + str(rand.rand()) + " | " + str(rand.rand()))
 
+    print('-' * 80)
+    ########################################
 
-    """for i in iv1:
-        srand(i)
-        r = rand()
-        print("i: " + str(i) + " | iv0: " + str((i // 65536) % 32768))
-        print("i: " + str(i) + " | iv1: " + str(r))
+    key3 = []
+    for i in iv0:
+        inva = modinv(1103515245, 2**32)
+        tmp = inva * (i - 12345) % 2**32
+        key3.append(tmp)
 
-    print('-' * 80)."""
-
-
-
-    """key3 = search(iv0, iv[0])
     print("key3: " + str(key3))
     print('-' * 80)
 
     for i in key3:
-        srand(i)
-        print("i: " + str(i))
-        print("rand1: " + str(rand()))
-        print("rand2: " + str(rand()))
+        rand.srand(i)
+        print(str((i // 2**16) % 2**15) + " | " + str(rand.rand()) + " | " + str(rand.rand()) + " | " + str(rand.rand()))
 
-    key2 = search(key3, key[3])
+    print('-' * 80)
+    ########################################
+
+    key2 = []
+    for i in key3:
+        inva = modinv(1103515245, 2**32)
+        tmp = inva * (i - 12345) % 2**32
+        key2.append(tmp)
+
     print("key2: " + str(key2))
     print('-' * 80)
 
-    key1 = search(key2, key[2])
+    for i in key2:
+        rand.srand(i)
+        print(str((i // 2**16) % 2**15) + " | " + str(rand.rand()) + " | " + str(rand.rand()) + " | " + str(rand.rand()) + " | " + str(rand.rand()))
+
+    print('-' * 80)
+    ########################################
+
+    key1 = []
+    for i in key2:
+        inva = modinv(1103515245, 2**32)
+        tmp = inva * (i - 12345) % 2**32
+        key1.append(tmp)
+
     print("key1: " + str(key1))
     print('-' * 80)
 
-    key0 = search(key1, key[1])
+    for i in key1:
+        rand.srand(i)
+        print(str((i // 2**16) % 2**15) + " | " + str(rand.rand()) + " | " + str(rand.rand()) + " | " + str(rand.rand()) + " | " + str(rand.rand()) + " | " + str(rand.rand()))
+
+    print('-' * 80)
+    ########################################
+
+    key0 = []
+    for i in key1:
+        inva = modinv(1103515245, 2**32)
+        tmp = inva * (i - 12345) % 2**32
+        key0.append(tmp)
+
     print("key0: " + str(key0))
     print('-' * 80)
 
-    for seed in key0:
-        if(is_good_seed(seed, key, iv)):
-            print("Good seed: " + seed)
-            break"""
+    rand.srand(key0[0])
+    keys0 = [rand.rand(), rand.rand(), rand.rand(), rand.rand()]
+    print(keys0)
+    rand.srand(key0[1])
+    keys1 = [rand.rand(), rand.rand(), rand.rand(), rand.rand()]
+
+    parameters = { 'key': keys0 }
+    response = server.query('/validation/sommerard', parameters)
+    print(response)
